d1adbe928138d5686f0634b5f4757fd050667dcf
obaydah bouifadene <obaydah.bouifadene@um5r.ac.ma>
2022-08-27 17:40:14 +0100

    playground

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -21,13 +21,14 @@ public class Main {
 //        TestClass.class.getDeclaredField("b").setAccessible(true);
 //        TestClass.class.getDeclaredField("b").set(testClass, 1);
         System.out.println(testClass);
+
 //        for (var i : Arrays.stream(TestClass.class.getDeclaredFields()).map(Field::getGenericType).toList()){
 //            testClass.
 //        }
         for (var method : Arrays.stream(TestClass.class.getDeclaredMethods()).filter(method -> method.getName().startsWith("set")).toList()) {
             try {
                 Class<?> type = method.getParameterTypes()[0];
-                System.out.println(type.getName());
+                System.out.println(type == Character.class);
                 method.invoke(testClass, randomGenerator.getRandomObject(type));
                 System.out.println(testClass);
 //                break;
@@ -35,6 +36,7 @@ public class Main {
                 e.printStackTrace();
             }
         }
+//
 
     }
 
