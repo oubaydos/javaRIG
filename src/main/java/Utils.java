bc65e9891866f876fc3e85f490947523a987abf3
mosle77 <ismail_mosleh@um5.ac.ma>
2022-09-17 10:10:20 +0100

    add unit tests

--- src/main/java/Utils.java
+++ src/main/java/Utils.java
@@ -1,6 +1,31 @@
+import java.lang.reflect.Type;
+
+
 public class Utils {
     public static String getFieldNameFromSetterMethodName(String setterMethodName){
         String fieldName = setterMethodName.replace("set","");
         return Character.toLowerCase(fieldName.charAt(0))+fieldName.substring(1);
+    }
+    public static boolean checkObjectHasValidateTypeAndNotEmpty(Type type,Object object){
+        TypeEnum typeEnum = TypeEnum.fromType(type);
+        return switch (typeEnum) {
+            case INTEGER -> (Integer) object <Integer.MAX_VALUE;
+            case STRING -> !((String) object).isEmpty();
+            case BYTE -> true;
+            case BYTE_ARRAY -> true;
+            case SHORT -> (Short) object <Short.MAX_VALUE;
+            case LONG -> (Long) object <Long.MAX_VALUE;
+            case DOUBLE -> (Double) object <Double.MAX_VALUE;
+            case FLOAT -> (Float) object <Float.MAX_VALUE;
+            case BOOLEAN -> !((Boolean) object).describeConstable().isEmpty();
+            case CHAR -> true;
+            case INSTANT -> true;
+            case DATE -> true;
+            case LOCAL_DATE -> true;
+            case MAP -> true;
+            case LIST -> true;
+            case ENUM -> true;
+            case UNDEFINED -> false;
+        };
     }
 }
--- src/test/java/RandomGeneratorTest.java
+++ src/test/java/RandomGeneratorTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
@@ -9,7 +10,6 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 class RandomGeneratorTest {
     RandomGenerator randomGenerator;
     TestClass testClass;
@@ -21,16 +21,22 @@ class RandomGeneratorTest {
     }
 
     @Test
-    void generateRandomObjectForType() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchFieldException {
+    void generateRandomObjectForType() throws  InvocationTargetException, IllegalAccessException, NoSuchFieldException {
+        Object object;
         for (var method : Arrays.stream(TestClass.class.getDeclaredMethods()).filter(method -> method.getName().startsWith("set")).toList()) {
             Field field = TestClass.class.getDeclaredField(Utils.getFieldNameFromSetterMethodName(method.getName()));
             Type type = field.getGenericType();
-            method.invoke(testClass, randomGenerator.generateRandomObjectForType(type));
+            object= randomGenerator.generateRandomObjectForType(type);
+//            System.out.println(object);
+            assertTrue(Utils.checkObjectHasValidateTypeAndNotEmpty(type,object));
+//            System.out.println(object.getClass());
+            //            method.invoke(testClass, randomGenerator.generateRandomObjectForType(type));
         }
     }
 
     @Test
     void generateRandomObject() throws NoSuchFieldException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
-        TestClass TestClass=randomGenerator.generateRandomObject(TestClass.class);
+        TestClass testClass1=randomGenerator.generateRandomObject(TestClass.class);
+
     }
 }
