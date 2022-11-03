278ab960ff0781fb168766047efbbd5d2c79ee5b
HamzaBenyazid <hamzabyazid@gmail.com>
2022-11-03 14:15:13 +0100

    suppress casting warning and change a variable name

--- src/main/java/io/javarig/RandomGeneratorV2.java
+++ src/main/java/io/javarig/RandomGeneratorV2.java
@@ -11,13 +11,14 @@ import java.util.function.Consumer;
 public class RandomGeneratorV2 {
     private final Stack<Type> objectStack = new Stack<>();
 
-    private <T> T generate(Type type, Consumer<CollectionGenerator> consumer) {
+    @SuppressWarnings({"unchecked"})
+    private <T> T generate(Type type, Consumer<CollectionGenerator> setCollectionSize) {
         checkForRecursion(type);
         objectStack.push(type);
         TypeEnum typeEnum = TypeEnum.fromType(type);
         Generator generator = typeEnum.generator();
         if (generator instanceof CollectionGenerator collectionGenerator) {
-            consumer.accept(collectionGenerator);
+            setCollectionSize.accept(collectionGenerator);
         }
         T generated = (T) generator.generate(type);
         objectStack.pop();
@@ -40,10 +41,12 @@ public class RandomGeneratorV2 {
         });
     }
 
-
-    //check if type exists in objectStack, if so then object can't be generated because there is recursion
-    // in this object's fields (there a field that it's instantiation depends on a father object)
-    //so NestedObjectRecursion is thrown
+    /**
+     * check if type exists in objectStack, if so then object can't be generated because there is recursion
+     * in this object's fields (there a field that it's instantiation depends on a father object)
+     * so NestedObjectRecursion is thrown
+     * @param type - a type to search for in the stack
+     */
     private void checkForRecursion(Type type) {
         if (!objectStack.isEmpty() && objectStack.contains(type)) {
             throw new NestedObjectRecursionException(type);
