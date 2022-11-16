fa83fa5c0bb5af6febcea225d10efbab387a328e
HamzaBenyazid <hamzabyazid@gmail.com>
2022-11-16 20:41:22 +0100

    change LocalDateGenerator.generate() return type

--- src/main/java/io/javarig/generator/LocalDateGenerator.java
+++ src/main/java/io/javarig/generator/LocalDateGenerator.java
@@ -1,12 +1,13 @@
 package io.javarig.generator;
 
 import java.time.Instant;
+import java.time.LocalDate;
 
 import static java.time.ZoneOffset.UTC;
 
 public class LocalDateGenerator implements Generator {
     @Override
-    public Object generate() {
+    public LocalDate generate() {
         return ((Instant) randomGenerator.generate(Instant.class)).atZone(UTC).toLocalDate();
     }
 }
