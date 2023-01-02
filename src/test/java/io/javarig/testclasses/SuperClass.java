52f6455473fe157e800c15c042661743b5962d1c
obaydah bouifadene <obaydah.bouifadene@um5r.ac.ma>
2023-01-02 17:50:03 +0100

    added classes for testing

---
+++ src/test/java/io/javarig/testclasses/SuperClass.java
@@ -0,0 +1,12 @@
+package io.javarig.testclasses;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Setter
+@Getter
+public class SuperClass {
+    Float inheritedField;
+}
