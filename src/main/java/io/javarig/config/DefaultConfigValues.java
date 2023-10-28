864f94fb34da4f012107cb27ba6218f4ead0af2d
obaydah bouifadene <obaydah.bouifadene@um5r.ac.ma>
2023-10-28 20:26:25 +0200

    added special characters to default regex

--- src/main/java/io/javarig/config/DefaultConfigValues.java
+++ src/main/java/io/javarig/config/DefaultConfigValues.java
@@ -3,5 +3,5 @@ package io.javarig.config;
 public class DefaultConfigValues {
     public final static int DEFAULT_MAX_SIZE_EXCLUSIVE = 15;
     public final static int DEFAULT_MIN_SIZE_INCLUSIVE = 5;
-    public final static String DEFAULT_REGEX_PATTERN = "[a-zA-Z0-9]*";
+    public final static String DEFAULT_REGEX_PATTERN = "[a-zA-Z0-9?!%&][,.;/*+-_#^@(){}|'<>]*";
 }
