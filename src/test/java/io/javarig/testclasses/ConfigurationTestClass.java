c62263c4621e0aba50a9542c3a14ee79014f9468
Hamza Ben Yazid <hamzabyazid@gmail.com>
2023-11-07 16:00:46 +0100

    write some tests for Configurations

---
+++ src/test/java/io/javarig/testclasses/ConfigurationTestClass.java
@@ -0,0 +1,11 @@
+package io.javarig.testclasses;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class ConfigurationTestClass {
+    private String s;
+    private List<Integer> l; 
+}
